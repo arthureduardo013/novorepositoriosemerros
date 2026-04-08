b30f545a0a542bf50bd6d55f0539919bf5b5dba4
arthureduardo013 <202601323482@alunos.estacio.br>
2026-04-08 13:39:49 -0300

    feature: atualizaçao

--- modificadoresdedados.exe/modificadoresdedados.c
+++ modificadoresdedados.exe/modificadoresdedados.c
@@ -7,5 +7,12 @@ int main() {
     printf("Número com sinal: %d\n", numeroSinal);
     printf("Número sem sinal: %u\n", numeroSemSinal);
 
+    int numeronormal = 2147483647;
+    long int NumeroGrande = 2147483647;
+
+    printf("Numero regular(int): %d\n",numeronormal);
+    printf("Numero Grande (long int): %d\n",NumeroGrande);
+
+
     return 0;
 }
