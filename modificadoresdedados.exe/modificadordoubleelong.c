05dc991489406c9ea6d175871bd2c4634e4961b9
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-03 14:09:45 -0300

    feature: atualização.

---
+++ modificadoresdedados.exe/modificadordoubleelong.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    double numeroPreciso = 3.141592653589793238463;
+    long double numeroMuitoPreciso = 3.141592653589793238463;
+
+    printf("Numero preciso (double): %.15f\n", numeroPreciso);
+    printf("Numero muito preciso (long double): %.21Lf\n", numeroMuitoPreciso);
+    return 0;
+}
