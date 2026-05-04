67eaa49ac5c807e0f03be83d8817f09f7e96c5fa
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-04 13:17:35 -0300

    feature: atualização.

--- modificadoresdedados.exe/comoosperadoresagemdeformaexplicita.c
+++ modificadoresdedados.exe/comoosperadoresagemdeformaexplicita.c
@@ -2,11 +2,13 @@
 int main(){
     int x = 5;
     float y = 5.0;
-
+    char c = 'a';
     //char c = 'a';
 
     printf("x >= y: %d\n", x >= y);
     printf("x == y: %d\n", x == y);
     printf("x != y: %d\n", x != y);
     
+    printf("x >= c : %d\n", x >= c);
+    printf("o valor de ASCII de %c é %d: \n", c, c);
 }
