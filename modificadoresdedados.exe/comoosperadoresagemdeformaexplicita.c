c6bd872e9d5b1009e6676d793a430b2e74c7cbab
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-04 13:12:02 -0300

    Create comoosperadoresagemdeformaexplicita.c

---
+++ modificadoresdedados.exe/comoosperadoresagemdeformaexplicita.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int x = 5;
+    float y = 5.0;
+
+    //char c = 'a';
+
+    printf("x >= y: %d\n", x >= y);
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+    
+}
