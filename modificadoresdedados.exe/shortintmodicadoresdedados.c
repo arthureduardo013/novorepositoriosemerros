d4234635a701029092d6c8af6db4c88fffe577e1
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-04 13:04:23 -0300

    feature: atualização.

--- modificadoresdedados.exe/shortintmodicadoresdedados.c
+++ modificadoresdedados.exe/shortintmodicadoresdedados.c
@@ -7,6 +7,6 @@ numeroPequeno = 32767; // valor maior que o máximo de short int
 printf("Numero pequeno atualizado (short int): %d\n", numeroPequeno);
 
 // printf("\n*** tamanho das variaveis ***\n");
-// printf("short int: %")
+// printf("short int: %lu B - long int")
 
 }
