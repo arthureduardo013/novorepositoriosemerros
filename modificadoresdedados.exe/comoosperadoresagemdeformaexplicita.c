b738d559ed79027019e4952f7f78da97d5cc4427
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-04 13:38:41 -0300

    feature: atualização.

--- modificadoresdedados.exe/comoosperadoresagemdeformaexplicita.c
+++ modificadoresdedados.exe/comoosperadoresagemdeformaexplicita.c
@@ -3,8 +3,8 @@ int main(){
     float  numero1 = 10.2;
     int numero2 = 10;
 
-    printf("numero1 > numero2: %d\n", numero1 > numero2);
-    printf("numero1 == numero2: %d\n", numero1 == numero2);
+    printf("numero1 > numero2: %d\n", (int)numero1 > numero2);
+    printf("numero1 == numero2: %d\n", (int)numero1 == numero2);
 
     return 0;
 }
