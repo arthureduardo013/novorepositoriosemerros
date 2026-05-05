b15eab9522ab9189526147d663e4bc609a1b7bc5
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-05 12:11:03 -0300

    feature: atualização.

---
+++ .vscode/horadecodarnivelmestregerenciamento/horadecodarnivelmestre.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    //decalrar variaveis produto, u i estoque, double valor unitario, 
+    // u i quantidade minima
+    char produtoA[30] = "produto A";
+    char produtoB[30] = "produto B";
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valortotalA;
+    double valortotalB;
+
+}
