5872d66345d5bc9053fe28e97ef23ea62e92ade2
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-06 13:55:57 -0300

    feaure: atualização.

--- modificadoresdedados.exe/estruturadedecisaosimplesIF.c/comparaçãoidadesIF.c
+++ modificadoresdedados.exe/estruturadedecisaosimplesIF.c/comparaçãoidadesIF.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 int main(){
 
     int idade1 = 25;
     int idade2 = 30;
 
-    if(idade1 > idade2) {
+    if (idade1 > idade2) {
         printf("pessoa 1 é mais velha que a pessoa 2\n");
     }
 }
