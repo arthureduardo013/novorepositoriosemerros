56f50ea6c891bf283d070e47feac933a69fd8473
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-06 13:32:36 -0300

    feature: atualização.

---
+++ modificadoresdedados.exe/estruturadedecisaosimplesIF.c/verificandoseapessoaemaiordeidadecomIF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 17;
+
+    if(idade >= 18);{
+        printf("você é maior de idade\n");
+        printf("dentro IF\n");
+    }
+        printf("fora IF\n");
+    
+}
