b7eb1f934c7a14c572c2c3a72a4556738db8833c
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-06 14:11:17 -0300

    feature: atualização.

--- modificadoresdedados.exe/comparaçãodeidadecomIF/comparaidadeIF.c
+++ modificadoresdedados.exe/comparaçãodeidadecomIF/comparaidadeIF.c
@@ -5,7 +5,7 @@ int main(){
    int idade1 = 25;
    int idade2 = 30;
 
-   if (idade1 > idade2 ) {
+   if (idade1 < idade2 ) {
     printf("a pessoa1 e maior que a pessoa2");
    }
    printf("fora do IF");
---
+++ modificadoresdedados.exe/comparaçãodeidadecomIF/comparaquememaisvelhocomIF.exe/comparaçãodeidadecomIFagoracerto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+int idade1 = 35;
+int idade2 = 30;
+
+if ( idade1 < idade2) {
+    printf("pessoa 1 é mais nova que a pessoa 2\n");
+}
+
+}
