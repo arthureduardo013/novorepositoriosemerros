d4a3b207e490473af22b59a4f5a909c9149bba7f
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-06 14:03:24 -0300

    feature: atualização.

---
+++ modificadoresdedados.exe/comparaçãodeidadecomIF.c/comparaidadeIF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+   int idade1 = 25;
+   int idade2 = 30;
+
+   if (idade1 > idade2 ) {
+    printf("a pessoa1 e maior que a pessoa2");
+   }
+   printf("fora do IF");
+}
