6e8cbf845372016546a1c402654f034c209df86c
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-14 13:30:04 -0300

    feature: atualização.

--- modificadoresdedados.exe/comparaçãodeidadecomIF/estruturadedecisaocomposta.c/estruturadedecisaocompostaIFeELSE.c
+++ modificadoresdedados.exe/comparaçãodeidadecomIF/estruturadedecisaocomposta.c/estruturadedecisaocompostaIFeELSE.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main(){
-
-int numero = 11;
-
-
-if (numero % 2 == 0) {
-    printf("o numero e par\n");
-}    else {
-    printf("o numero e impar\n");
+    int numero = 11;
+    if (numero = % 2 == 0) {
+        printf("o numero e par\n");
+    } else {
+        printf("o numero e impar\n");
     }
 }
 
