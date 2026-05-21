8fdfeb552252daf4f38e1309cabd027ff00a6bb8
arthureduardo013 <202601323482@alunos.estacio.br>
2026-05-20 22:08:04 -0300

    feature: atualização.

---
+++ IFeELSEnapratica/operadoreslogicos/estruturadedecisaoEncadeadas/operadorternarioPasta/MovimentacoesdaspeçasdeXADREZ/WHILEnapratica.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 1; //variavel de incremento
+
+    while (i <= 5) { //condição
+        printf("%d\n", i); //saida
+        i++; //incremento
+
+    }
+
+    return 0;
+}
